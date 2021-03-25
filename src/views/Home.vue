1b2bec09479d083e8bd667c3b7480d31eaf013b6
Lee Goddard <leegee@gmail.com>
2021-03-25 11:01:00 +0100

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,218 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <form>
+      <div class="column">
+        <fieldset>
+          <p>
+            <label
+              >Constants
+              <small>
+                <code>#define name value</code> &mdash; one per line
+              </small>
+              <textarea id="variables" v-model="settings.variables"></textarea>
+            </label>
+          </p>
+
+          <p>
+            <label
+              >Rules
+              <small>
+                <code>F(x,y): x==1 && y==2 => NewPattern</code>
+                &mdash; one rule per line
+              </small>
+            </label>
+            <textarea id="rules" v-model="settings.rules"></textarea>
+          </p>
+
+          <p>
+            <label
+              >Axiom<input type="text" id="start" v-model="settings.start" />
+            </label>
+          </p>
+
+          <p>
+            <label
+              >Angle
+              <input
+                type="range"
+                id="angle"
+                min="0"
+                max="360"
+                aria-valuemin="0"
+                aria-valuemax="360"
+                v-model="settings.angle"
+              />
+            </label>
+
+            <label
+              >Generations
+              <input
+                type="range"
+                min="1"
+                max="10"
+                aria-valuemin="1"
+                aria-valuemax="10"
+                id="totalGenerations"
+                v-model="settings.totalGenerations"
+              />
+            </label>
+          </p>
+        </fieldset>
+
+        <input
+          type="button"
+          id="actionGenerate"
+          value="🖼 Regenerate Image & MIDI"
+          @click="actionGenerate"
+        />
+        <!-- <input type="button" id="actionCreateMidi" value="🎼Regenerate MIDI" /> -->
+      </div>
+
+      <div class="column">
+        <fieldset>
+          <legend>MIDI</legend>
+          <p>
+            <label
+              >Allow notes to be placed back in the past?
+              <input
+                type="checkbox"
+                id="backInTime"
+                v-model="settings.backInTime"
+              />
+            </label>
+          </p>
+
+          <p>
+            <label
+              >Note duration
+              <input
+                type="range"
+                min="1"
+                max="250"
+                id="duration"
+                v-model="settings.duration"
+              />
+            </label>
+
+            <!-- <label>Initial note <input type='range' min=0 max=127 step=1 value="64" id='initialNote'>
+                                </label> -->
+          </p>
+
+          <p>
+            <label
+              >Scale
+              <select v-model="scale">
+                <option
+                  v-for="option in scales"
+                  :value="option"
+                  v-bind:key="option"
+                >
+                  {{ option }}
+                </option>
+              </select>
+            </label>
+          </p>
+
+          <p>
+            <label
+              >Wrap at angle
+              <input
+                type="range"
+                id="wrapAngleAt"
+                min="0"
+                max="360"
+                aria-valuemin="0"
+                aria-valuemax="360"
+                v-model="settings.wrapAngleAt"
+              />
+            </label>
+          </p>
+        </fieldset>
+
+        <fieldset>
+          <legend>Output</legend>
+          <textarea
+            id="contentDisplay"
+            v-model="settings.contentDisplay"
+          ></textarea>
+        </fieldset>
+
+        <div id="player"></div>
+      </div>
+    </form>
+
+    <div ref="canvases"></div>
+  </section>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style scoped>
+.column {
+  width: 50%;
+  float: left;
+}
+textarea {
+  width: 100%;
+}
+</style>
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<script>
+import { Vue, Options } from "vue-class-component";
+import Presets, { defaultPresettings } from "@/lib/Presets";
+import { scale as tonalScales } from "tonal";
+
+import logger from "@/lib/gui/Logger";
+// import LsysParametric from '../LsysParametric';
+import LsysRenderer from "@/lib/gui/LsysRenderer";
+
+@Options({
+  components: {},
+})
+export default class Home extends Vue {
+  settings = {};
+  scales = tonalScales;
+
+  mounted() {
+    this.loadPreset(0);
+  }
+
+  actionGenerate() {
+    logger.debug("Enter actionGenerate");
+    const canvas = window.document.createElement("canvas");
+    this.$refs.canvases.insertBefore(canvas, this.$refs.canvases.firstChild);
+
+    this.lsysRenderer = new LsysRenderer(this.settings, canvas, logger);
+
+    canvas.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+
+    logger.silly("Call service to start Lsys with", this.settings);
+    this.service("start", this.settings);
+  }
+
+  loadPreset(idx = 0) {
+    logger.info("Load preset ", idx, Presets[idx]);
+
+    if (!Presets[idx].totalGenerations) {
+      this.logger.warn(
+        "Preset %d had no value for totalGenerations: using 1.",
+        idx
+      );
+      Presets[idx].totalGenerations = 1;
+    }
+
+    this.settings = {
+      ...defaultPresettings,
+      ...this.settings,
+      ...Presets[idx],
+    };
+
+    this.actionGenerate();
   }
 }
 </script>
+
+
+
+
